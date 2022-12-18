a30bb7aa0b089986a9bb3f6f93a188128b8a988a
jokay03J <magrojoan3@gmail.com>
2022-12-18 23:29:22 +0100

    add route and base file App

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+import { RouterProvider } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home/Home";
+
+function App() {
+  const router = createBrowserRouter([{ path: "", element: <Home></Home> }]);
+
+  return <RouterProvider router={router}></RouterProvider>;
+}
+
+export default App;
