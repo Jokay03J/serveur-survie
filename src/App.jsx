05b7fb952c894790a501aaef041f1a38da2297e2
jokay03J <magrojoan3@gmail.com>
2022-12-18 23:42:10 +0100

    fix incorrect router

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home/Home";
 
 function App() {
-  const router = createBrowserRouter([{ path: "", element: <Home></Home> }]);
+  const router = createBrowserRouter([
+    { path: "/serveur-survie/", element: <Home></Home> },
+  ]);
 
   return <RouterProvider router={router}></RouterProvider>;
 }
