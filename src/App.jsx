b1234381a4ff98c79180fc0ef8f6a5f24c94ab06
jokay03J <magrojoan3@gmail.com>
2022-12-19 18:04:38 +0100

    change router config

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import "./App.scss";
 import { RouterProvider } from "react-router-dom";
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import NotFound from "./pages/NotFound/NotFound";
 
 function App() {
   const router = createBrowserRouter([
-    { path: "/serveur-survie/", element: <Home></Home> },
+    { path: "/", element: <Home></Home> },
+    { path: "*", element: <NotFound></NotFound> },
   ]);
 
   return <RouterProvider router={router}></RouterProvider>;
